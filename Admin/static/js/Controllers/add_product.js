0c06f79a5e45d5068ec0a2762c83e4e4281fde1d
sahilsehgal1995 <sahilsehgal1995@gmail.com>
2016-03-04 15:27:58 +0530

    Add Product

--- Admin/static/js/Controllers/add_product.js
+++ Admin/static/js/Controllers/add_product.js
@@ -1,14 +1,28 @@
 (function() {
   var app = angular.module('ShopKare_Backend-master', []);
-// var base = 'http://127.0.0.1:5000';
+//   var base = 'http://127.0.0.1:5000';
   var base = 'http://shopkare.com';
+
   var categories = [];
   var level1keys= [];
-    app.controller('add_new_product',function($scope,$http){
+  var ProductQuantityArray= {};
+    app.controller('add_new_product',['$scope', '$http',function($scope,$http){
+      console.log("CONtroller Working");
+     
+      /*$http.post("GETSESSIONCITY")
+      .success(function(response){
+        $scope.city=response;
+      })
+      .error(function(response){
+        console.log("response");
+      });
+*/  
+      $scope.city="Hyderabad";
       $scope.Product={};
       $scope.index1=0;
-      $scope.Product.Quantity=[{"Quantity":["",""]}];
-       $http.get("http://shopkare.com/api/Admin/addProduct?user="+JSON.stringify($scope.Product))
+      $scope.quantities=[{"Quantity":["",""]}];
+      console.log($scope.city);
+      $http.get(base+"/api/Admin/addProduct?user="+JSON.stringify($scope.Product))
        .then(function(response){
         $scope.main_level_products=response['data'];
        });
@@ -16,18 +30,74 @@
        $scope.button_function=function(){
         console.log("Function Called Successfully");
         $scope.index1=$scope.index1+1;
-      $scope.dataa={"Quantity": [" "," "]};
-        $scope.Product.Quantity.push($scope.dataa);
+	$scope.dataa={"Quantity": [" "," "]};
+        $scope.quantities.push($scope.dataa);
        };
-       
+
+	$scope.getFileDetails = function (e) {
+
+            $scope.files = [];
+            $scope.$apply(function () {
+
+                // STORE THE FILE OBJECT IN AN ARRAY.
+                for (var i = 0; i < e.files.length; i++) {
+                    $scope.files.push(e.files[i])
+                }
+
+            });
+        };
+       		// NOW UPLOAD THE FILES.
+        $scope.uploadFiles = function () {
+
+            //FILL FormData WITH FILE DETAILS.
+	    console.log("Sumitted");
+            var data = new FormData();
+            console.log($scope.files);
+            for (var i in $scope.files) {
+                data.append("uploadedFile", $scope.files[i]);
+            }
+
+            // ADD LISTENERS.
+            var objXhr = new XMLHttpRequest();
+            objXhr.addEventListener("progress", updateProgress, false);
+            objXhr.addEventListener("load", transferComplete, false);
+	    
+	  //add Product details in Product
+	    $scope.addProduct();
+
+            // SEND FILE DETAILS TO THE API.
+            objXhr.open("POST", base+"/api/Admin/addProduct/?product=JSON.stringify($scope.Product)");
+            objXhr.send(data);
+        }
+
+        // UPDATE PROGRESS BAR.
+        function updateProgress(e) {
+            if (e.lengthComputable) {
+                document.getElementById('pro').setAttribute('value', e.loaded);
+                document.getElementById('pro').setAttribute('max', e.total);
+            }
+        }
+
+        // CONFIRMATION.
+        function transferComplete(e) {
+            alert("Files uploaded successfully."+ e);
+        }
+               
+     
       $scope.addProduct = function()
-      {
-	console.log(getproducts());
+      { 
+    
+      ProductQuantityArray['City']=$scope.city; 
+      ProductQuantityArray['Quantities']=$scope.quantities; 
+	//console.log(getproducts());  
 	$scope.Product['_id'] = getproducts();
+	$scope.Product['Quantity']=ProductQuantityArray;
 	$scope.Product['Level1 Category'] = JSON.parse($scope.l1category)._id;
 	$scope.Product['Main Category'] = $scope.mainCategories[$scope.mainCategory];
 	$scope.Product['Sub Category'] = $scope.subcategories[$scope.subcategoryindex];
 	console.log(JSON.stringify($scope.Product));
+	
+	
 	$http.post(base+'/api/Admin/addProduct/?product='+JSON.stringify($scope.Product))
 	.success(function(response){
 	  $scope.reply=response;
@@ -45,6 +115,9 @@
 	  console.log(error);
 	})
 	;
+	
+	
+	
       };
        $scope.gettablenames=function(){
         console.log("function Called Successfully");
@@ -102,6 +175,6 @@
       var id = level1keys.indexOf(JSON.parse($scope.l1category)._id).toString() +'_'+ $scope.mainCategory.toString() + '_'+ $scope.subcategoryindex.toString();
       return id;
     };
-    
-    });
+   
+    }]);
 })();
